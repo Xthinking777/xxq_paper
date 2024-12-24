8b389dd5f46a9c582fab13900f928902ab795cc3
Xthinking777 <2827060313@qq.com>
2024-12-24 11:32:14 +0800

    Sat_sim

--- Sat_Nonliner/Sat_Model_G.m
+++ Sat_Nonliner/Sat_Model_G.m
@@ -2,7 +2,8 @@
 clear
 Data_Am=20000;%Amount of output data for close-loop system
 Data_G=300;%Closed loop impulse response truncated data
-sim('gauss');
+
+%sim('gauss');
 %%定义模型
 KZQ_1=filt([2.841 -4.406 1.749],[1 -1]); 
 T_1=filt([zeros(1,5) 0.2],[1 -0.8]);
@@ -13,28 +14,64 @@ KZQ=KZQ_1;
 T=T_1;
 N=N_1;
 
+
+sat_alpha=5;
+sim('Sat_Nonliner');
+
+%%饱和度
+sat_num=0;
+for i=1:1:50
+    if(abs(u_k(i))>sat_alpha) 
+        sat_num=sat_num+1;
+    end
+end
+Sat_percent=sat_num/50*100;
+
+
 %%FCOR
-y_out=Fcor(KZQ,T,N,y_a);%Generate output data
-g_est=fc(y_out(1:Data_Am));%Fcor
+% y_out=Fcor(KZQ,T,N,y_a);%Generate output data
+% g_est=fc(y_out(1:Data_Am));%Fcor
+y_out=y_k(1:Data_Am);
+g_est=fc(y_out);%Fcor
+
+
 G_est=filt(g_est(1:Data_G),1);
 G_theo=N/(1+T*KZQ);
 
 
 %%画图
 % 计算理论脉冲响应和估计脉冲响应
-G_theo_i = impulse(G_theo, 50);
-G_est_i = impulse(G_est, 50);
-
-% 创建时间向量，从 0 到 99（因为 impulse 返回 100 个点）
+G_theo_i  = impulse(G_theo, 50);
+G_est_i   = impulse(G_est, 50);
+u_k_i     = u_k(1:51);
+sat_u_k_i = sat_u_k(1:51);
+y_k_i     = y_k(1:51);
+% 创建时间向量，从 0 到 50（因为 impulse 返回 100 个点）
 t = (0:length(G_theo_i)-1)'; % 转置以确保 t 是列向量
 
-% 绘制脉冲响应对比图
+
+% 绘制脉冲响应对比图1
+figure; % 创建一个新的图形窗口
+plot(t, u_k_i, 'r--', 'LineWidth', 2); % 绘制估计脉冲响应，红色虚线
+hold on; % 保持当前图形，以便在同一图上绘制
+plot(t, sat_u_k_i, 'b--', 'LineWidth', 2); % 绘制估计脉冲响应，红色虚线
+hold on; % 保持当前图形，以便在同一图上绘制
+plot(t, y_k_i, 'g--', 'LineWidth', 2); % 绘制估计脉冲响应，红色虚线
+hold off; % 不再保持图形
+% 添加图例和标签
+legend('u_k', 'sat_u_k','y_k');
+xlabel('时间 (样本)');
+ylabel('响应幅度');
+title('脉冲响应对比图');
+grid on; % 添加网格
+
+
+% 绘制脉冲响应对比图2
 figure; % 创建一个新的图形窗口
 plot(t, G_theo_i, 'b', 'LineWidth', 2); % 绘制理论脉冲响应，蓝色线
 hold on; % 保持当前图形，以便在同一图上绘制
 plot(t, G_est_i, 'r--', 'LineWidth', 2); % 绘制估计脉冲响应，红色虚线
 hold off; % 不再保持图形
-
 % 添加图例和标签
 legend('理论脉冲响应', '估计脉冲响应');
 xlabel('时间 (样本)');
--- Sat_Nonliner/fc.m
+++ Sat_Nonliner/fc.m
@@ -14,7 +14,7 @@ function [ f_ya ] = fc( y )%系统闭环脉冲响应求解程序
 N=length(y);
 m1=armax(y,[20 20]);%A(q) y(t) = C(q) e(t)，其中设A为10阶；B为9阶；m1为AB模型且包含其信息
 e=resid(m1,y);%求序列y的残差
-close
+
 e_var=var(e);%求残差方差
 e=e*(1/sqrt(e_var));
 for r=0:1000
